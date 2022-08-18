4abd5510e418225d0d964c8d1265b3f20f7d6f42
GioF71 <giovanni.fulco@gmail.com>
2022-08-18 23:18:29 +0200

    working cid

--- freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
+++ freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
@@ -12,19 +12,19 @@ const table = [
 
 function checkCashRegister(price, cash, cid) {
   console.log(price, cash, cid);
-  const initial_cid = JSON.parse(JSON.stringify(cid)); 
+  const current_cid = JSON.parse(JSON.stringify(cid)); 
   let diff = cash - price;
   let change = [];
   let result = {};
   for (let i = table.length - 1; diff > 0 && i >= 0; --i) {
     const curr_unit = table[i][1];
     // if diff >= unit && some qty for this unit
-    if (diff >= curr_unit && cid[i][1] > 0) {
+    if (diff >= curr_unit && current_cid[i][1] > 0) {
       const needed_count = Math.floor(diff / curr_unit);
-      const avail_count = Math.round(cid[i][1] / curr_unit);
+      const avail_count = Math.round(current_cid[i][1] / curr_unit);
       const to_be_drawn_count = Math.min(needed_count, avail_count);
       const amount = to_be_drawn_count * curr_unit;
-      cid[i][1] = (avail_count - to_be_drawn_count) * curr_unit;
+      current_cid[i][1] = (avail_count - to_be_drawn_count) * curr_unit;
       diff = Math.round((diff - amount) * 100.0) / 100.0;
       change.push([table[i][0], amount]);
     }
@@ -32,9 +32,9 @@ function checkCashRegister(price, cash, cid) {
   // is diff == 0?
   if (diff == 0) {
     // closed?
-    const closed = cid.every(x => x[1] == 0);
+    const closed = current_cid.every(x => x[1] == 0);
     result["status"] = closed ? "CLOSED" : "OPEN";
-    result["change"] = closed ? initial_cid : change;
+    result["change"] = closed ? cid : change;
   } else {
     result["status"] = "INSUFFICIENT_FUNDS";
     result["change"] = [];
