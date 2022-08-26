3c3d9d4d5f060fddbda84c5472f5eeb709b397d0
GioF71 <giovanni.fulco@gmail.com>
2022-08-26 18:37:48 +0200

    using assertions

---
+++ assertions/assertions.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+
+function assertRegexTest(sentence, regex, bool_result = true) {
+    assert(regex.test(sentence) == bool_result);
+    console.log("Verified [", bool_result, "] for Regex [", regex, "] test on string [", sentence, "]");
+}
+
+const default_representer = (x) => { return String(x);};
+
+function assertEquals(
+        left, 
+        right, 
+        expected_result, 
+        representer_function = default_representer) {
+    const message = "Expecting [" + representer_function(left) + "] got [" + representer_function(right) + "]";
+    const comp_result = (left == right);
+    assert(comp_result === expected_result, message);
+}
+
+module.exports = { assertRegexTest, assertEquals};
--- freeCodeCamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/addTogether.js
+++ freeCodeCamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/addTogether.js
@@ -1,3 +1,6 @@
+const assert = require("assert");
+const assertions = require("../../../assertions/assertions");
+
 function isNumeric(n) {
   return typeof n == "number";
 }
@@ -16,11 +19,23 @@ function addTogether() {
   return a + arguments[1];
 }
 
-console.log(addTogether(2, 3)); //5
-console.log(addTogether(23, 30)); //53.
-console.log(addTogether(5)(7)); //12.
-console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ")); //undefined
-console.log(addTogether(2, "3")); //undefined
-console.log(addTogether(2)([3])); //undefined
-console.log(addTogether("2", 3)); //undefined
-console.log(addTogether(5, undefined)); //undefined
+//console.log(addTogether(2, 3)); //5
+//console.log(addTogether(23, 30)); //53.
+//console.log(addTogether(5)(7)); //12.
+//console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ")); //undefined
+//console.log(addTogether(2, "3")); //undefined
+//console.log(addTogether(2)([3])); //undefined
+//console.log(addTogether("2", 3)); //undefined
+//console.log(addTogether(5, undefined)); //undefined
+
+const verify = (left, right) => assertions.assertEquals(left, right, true);
+//const verify = (left, right) => assert(left == right);
+
+verify(addTogether(2, 3), 5);
+//verify(addTogether(2,3), 6); <== this assertion would fail
+verify(addTogether(23, 30), 53);
+verify(addTogether(5, 7), 12);
+verify(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ"), undefined);
+verify(addTogether(2)([3]), undefined);
+verify(addTogether(2, "3"), undefined);
+verify(addTogether(5, undefined), undefined);
