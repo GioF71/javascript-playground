c9ea053449a93531fb1a06028d23c66c9d2fb6bb
GioF71 <giovanni.fulco@gmail.com>
2022-09-08 09:25:15 +0200

    cleanup

--- freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
+++ freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
@@ -1,5 +1,4 @@
 const assert = require("assert");
-//const { kMaxLength } = require("buffer");
 
 const STATUS = {
   OPEN: "OPEN", 
@@ -54,32 +53,30 @@ function checkCashRegister(price, cash, cid) {
   if (diff == 0) {
     // closed?
     const closed = current_cid.every(x => x[1] == 0);
-    result["status"] = closed ? STATUS.CLOSED : STATUS.OPEN;
-    result["change"] = closed ? cid : change;
+    //result["status"] = closed ? STATUS.CLOSED : STATUS.OPEN;
+    result.status = closed ? STATUS.CLOSED : STATUS.OPEN;
+    //result["change"] = closed ? cid : change;
+    result.change = closed ? cid : change;
   } else {
-    result["status"] = STATUS.INSUFFICIENT_FUNDS;
-    result["change"] = [];
+    //result["status"] = STATUS.INSUFFICIENT_FUNDS;
+    result.status = STATUS.INSUFFICIENT_FUNDS;
+    //result["change"] = [];
+    result.change = [];
   }
   console.log(result);
   return result;
 }
 
 const isDeepEqual = (object1, object2) => {
-
   const objKeys1 = Object.keys(object1);
   const objKeys2 = Object.keys(object2);
-
   if (objKeys1.length !== objKeys2.length) return false;
-
-  for (var key of objKeys1) {
+  for (const key of objKeys1) {
     const value1 = object1[key];
     const value2 = object2[key];
-
     const isObjects = isObject(value1) && isObject(value2);
-
     if ((isObjects && !isDeepEqual(value1, value2)) ||
-      (!isObjects && value1 !== value2)
-    ) {
+        (!isObjects && value1 !== value2)) {
       return false;
     }
   }
