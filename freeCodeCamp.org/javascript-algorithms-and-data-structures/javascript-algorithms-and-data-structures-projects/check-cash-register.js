9b6c8bb88e2b2d3069d6f2ff9d88bbab5020a6d1
GioF71 <giovanni.fulco@gmail.com>
2022-09-08 14:58:10 +0200

    less rounding

--- freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
+++ freeCodeCamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/check-cash-register.js
@@ -32,22 +32,30 @@ const table = [
 
 const multiplier = 100; // lowest value is 0.01
 
-function checkCashRegister(price, cash, cid) {
+const amountMultiply = (nativeAmount, use_multiplier = multiplier) => {
+  const strAmount = (nativeAmount * use_multiplier).toFixed(0);
+  return parseInt(strAmount);
+}
+
+const checkCashRegister = (price, cash, cid) => {
   //console.log(price, cash, cid);
   const current_cid = JSON.parse(JSON.stringify(cid)); 
-  let m_diff = Math.round((cash - price) * multiplier);
+  let m_diff = amountMultiply(cash - price);
   let change = [];
   let result = {};
   for (let i = table.length - 1; m_diff > 0 && i >= 0; --i) {
     const curr_unit = table[i][1];
-    const m_curr_unit = multiplier * table[i][1];
+    const m_curr_unit = amountMultiply(table[i][1]);
     if (m_diff >= m_curr_unit) {
-      const unit_avail_count = Math.round((multiplier * current_cid[i][1]) / m_curr_unit);
+      const curr_avail = amountMultiply(current_cid[i][1]);
+      const unit_avail_count = curr_avail / m_curr_unit;
       if (unit_avail_count > 0) {
         const m_needed_count = Math.floor(m_diff / m_curr_unit);
         const drawn = Math.min(m_needed_count, unit_avail_count);
         const amount = drawn * curr_unit;
+        // back to float for cid output
         current_cid[i][1] = (unit_avail_count - drawn) * curr_unit;
+        // decrement difference and store
         m_diff -= (m_curr_unit * drawn);
         change.push([table[i][0], amount]);
       }
@@ -56,9 +64,9 @@ function checkCashRegister(price, cash, cid) {
   // is difference at zero?
   if (m_diff == 0) {
     // closed? close if all is at 0
-    const closed = current_cid.every(x => x[1] == 0);
-    result.status = closed ? STATUS.CLOSED : STATUS.OPEN;
-    result.change = closed ? cid : change;
+    const must_close = current_cid.every(x => x[1] == 0);
+    result.status = must_close ? STATUS.CLOSED : STATUS.OPEN;
+    result.change = must_close ? cid : change;
   } else {
     result.status = STATUS.INSUFFICIENT_FUNDS;
     result.change = [];
@@ -90,6 +98,14 @@ const compare_register = (left, right) => {
   return isDeepEqual(left, right);
 }
 
+assert(amountMultiply(0.5) === 50);
+assert(amountMultiply(1.01) === 101);
+assert(amountMultiply(10) === 1000);
+assert(amountMultiply(19.5) === 1950);
+assert(amountMultiply(3.26) === 326);
+assert(amountMultiply(3.261) === 326); // wrong input, still works, maybe improperly (?)
+assert(amountMultiply(3.261, 1000) === 3261); // would be a correct input if multiplier === 1000
+
 const t1 = {status: STATUS.OPEN, change: [[MONEY.QUARTER, 0.5]]};
 const v1 = checkCashRegister(19.5, 20, [[MONEY.PENNY, 1.01], [MONEY.NICKEL, 2.05], [MONEY.DIME, 3.1], [MONEY.QUARTER, 4.25], [MONEY.ONE, 90], [MONEY.FIVE, 55], [MONEY.TEN, 20], [MONEY.TWENTY, 60], [MONEY.ONE_HUNDRED, 100]]);
 assert(compare_register(t1, v1));
@@ -110,3 +126,4 @@ const t5 = {status: STATUS.CLOSED, change: [[MONEY.PENNY, 0.5], [MONEY.NICKEL, 0
 const v5 = checkCashRegister(19.5, 20, [[MONEY.PENNY, 0.5], [MONEY.NICKEL, 0], [MONEY.DIME, 0], [MONEY.QUARTER, 0], [MONEY.ONE, 0], [MONEY.FIVE, 0], [MONEY.TEN, 0], [MONEY.TWENTY, 0], [MONEY.ONE_HUNDRED, 0]]);
 assert(compare_register(t5, v5));
 
+console.log("finished");
